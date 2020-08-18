b1b09334b16ee6af7829a0d892cd9522abb52d04
Remco Horbach <remco.horbach@student.howest.be>
2020-08-18 15:51:26 +0200

    Add refference for solving error

---
+++ AITRemcoHorbach/ClientReceive.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AITRemcoHorbach
+{
+    public delegate void ReceviedMessage(string sender, string message);
+    public delegate void GotNames(object sender, List<string> names);
+
+    class ReceiveClient : ISendChatServiceCallback
+    {
+        public event ReceviedMessage ReceiveMsg;
+        public event GotNames NewNames;
+
+        InstanceContext inst = null;
+        SendChatServiceClient chatClient = null;
+
+        public void Start(ReceiveClient receiveClient, string name)
+        {
+            inst = new InstanceContext(receiveClient);
+            chatClient = new SendChatServiceClient(inst);
+            chatClient.Start(name);
+        }
+
+        public void SendMessage(string message, string sender, string receiver)
+        {
+            chatClient.SendMessage(message, sender, receiver);
+        }
+
+        public void Stop(string name)
+        {
+            chatClient.Stop(name);
+        }
+
+        void ISendChatServiceCallback.ReceiveMessage(string message, string receiver)
+        {
+            ReceiveMsg?.Invoke(receiver, message);
+        }
+
+        public void SendNames(string[] names)
+        {
+            NewNames?.Invoke(this, names.ToList());
+        }
+    }
+}
---
+++ AITRemcoHorbach/Service.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AITRemcoHorbach
+{
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    [System.ServiceModel.ServiceContractAttribute(ConfigurationName = "ChatService.ISendChatService", CallbackContract = typeof(ChatClient.ISendChatServiceCallback))]
+    public interface ISendChatService
+    {
+
+        [System.ServiceModel.OperationContractAttribute(IsOneWay = true, Action = "http://tempuri.org/ISendChatService/SendMessage")]
+        void SendMessage(string message, string sender, string receiver);
+
+        [System.ServiceModel.OperationContractAttribute(IsOneWay = true, Action = "http://tempuri.org/ISendChatService/Start")]
+        void Start(string Name);
+
+        [System.ServiceModel.OperationContractAttribute(IsOneWay = true, Action = "http://tempuri.org/ISendChatService/Stop")]
+        void Stop(string Name);
+    }
+
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    public interface ISendChatServiceCallback
+    {
+
+        [System.ServiceModel.OperationContractAttribute(IsOneWay = true, Action = "http://tempuri.org/ISendChatService/ReceiveMessage")]
+        void ReceiveMessage(string message, string receiver);
+
+        [System.ServiceModel.OperationContractAttribute(IsOneWay = true, Action = "http://tempuri.org/ISendChatService/SendNames")]
+        void SendNames(string[] names);
+    }
+
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    public interface ISendChatServiceChannel : ChatClient.ISendChatService, System.ServiceModel.IClientChannel
+    {
+    }
+
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    public partial class SendChatServiceClient : System.ServiceModel.DuplexClientBase<ChatClient.ISendChatService>, ChatClient.ISendChatService
+    {
+
+        public SendChatServiceClient(System.ServiceModel.InstanceContext callbackInstance) :
+                base(callbackInstance)
+        {
+        }
+
+        public SendChatServiceClient(System.ServiceModel.InstanceContext callbackInstance, string endpointConfigurationName) :
+                base(callbackInstance, endpointConfigurationName)
+        {
+        }
+
+        public SendChatServiceClient(System.ServiceModel.InstanceContext callbackInstance, string endpointConfigurationName, string remoteAddress) :
+                base(callbackInstance, endpointConfigurationName, remoteAddress)
+        {
+        }
+
+        public SendChatServiceClient(System.ServiceModel.InstanceContext callbackInstance, string endpointConfigurationName, System.ServiceModel.EndpointAddress remoteAddress) :
+                base(callbackInstance, endpointConfigurationName, remoteAddress)
+        {
+        }
+
+        public SendChatServiceClient(System.ServiceModel.InstanceContext callbackInstance, System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) :
+                base(callbackInstance, binding, remoteAddress)
+        {
+        }
+
+        public void SendMessage(string message, string sender, string receiver)
+        {
+            base.Channel.SendMessage(message, sender, receiver);
+        }
+
+        public void Start(string Name)
+        {
+            base.Channel.Start(Name);
+        }
+
+        public void Stop(string Name)
+        {
+            base.Channel.Stop(Name);
+        }
+    }
+}
